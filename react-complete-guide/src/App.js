f9a173de7e959f983560a6da60210ba0cf3296b5
jshhrrsn <joshharrison@atreides.ai>
2020-10-18 22:03:16 +0100

    Component state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,12 +4,20 @@ import Person from './Person/Person'
 import Icecream from './Person/Icecream'
 
 class App extends Component {
+  state = {
+    persons:  [
+      {name: 'Bob', age: 33},
+      {name: 'rob', age: 50},
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>I'm a React App</h1>
-        <Person name="bob" age="33"><Icecream flavour="chocolate"/></Person>
-        <Person name="rob" age="50">I'm a great guy!!</Person>
+        <button>Switch Name</button>
+        <Person name={this.get_name(0)} age={this.get_age(0)}><Icecream flavour="chocolate"/></Person>
+        <Person name={this.get_name(1)} age={this.get_age(1)}>I'm a great guy!!</Person>
       </div>
     );
     // Manually creating the above - the above JSX gets compiled to this
@@ -17,6 +25,13 @@ class App extends Component {
     // React.createElement(
     // 'h1', null, "I'm a React App!!!"));
   }
+get_age(ix) {
+  return this.state.persons[ix].age
+}
+get_name(ix) {
+  return this.state.persons[ix].name
+}
+
 }
 
 export default App;
