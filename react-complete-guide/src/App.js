e6946f09ae23bbcc186631f009e9a4707df07c75
jshhrrsn <joshharrison@atreides.ai>
2020-10-18 21:09:14 +0100

    added Person component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>I'm a React App</h1>
+        <Person />
       </div>
     );
+    // Manually creating the above - the above JSX gets compiled to this
+    // return React.createElement('div', {className: 'App'},
+    // React.createElement(
+    // 'h1', null, "I'm a React App!!!"));
   }
 }
 
