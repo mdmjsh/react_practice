e471885b0efd93ccf24c26a11a34b6636b49b208
jshhrrsn <joshharrison@atreides.ai>
2020-10-18 22:07:15 +0100

    a bit more state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -6,8 +6,8 @@ import Icecream from './Person/Icecream'
 class App extends Component {
   state = {
     persons:  [
-      {name: 'Bob', age: 33},
-      {name: 'rob', age: 50},
+      {name: 'Bob', age: 33, flavour: 'chocolate'},
+      {name: 'rob', age: 50, flavour: 'vanilla'},
     ]
   }
 
@@ -16,7 +16,7 @@ class App extends Component {
       <div className="App">
         <h1>I'm a React App</h1>
         <button>Switch Name</button>
-        <Person name={this.get_name(0)} age={this.get_age(0)}><Icecream flavour="chocolate"/></Person>
+        <Person name={this.get_name(0)} age={this.get_age(0)}><Icecream flavour={this.get_flavour(0)}/></Person>
         <Person name={this.get_name(1)} age={this.get_age(1)}>I'm a great guy!!</Person>
       </div>
     );
@@ -25,12 +25,15 @@ class App extends Component {
     // React.createElement(
     // 'h1', null, "I'm a React App!!!"));
   }
-get_age(ix) {
-  return this.state.persons[ix].age
-}
-get_name(ix) {
-  return this.state.persons[ix].name
-}
+  get_age(ix) {
+    return this.state.persons[ix].age
+  }
+  get_name(ix) {
+    return this.state.persons[ix].name
+  }
+  get_flavour(ix) {
+    return this.state.persons[ix].flavour
+  }
 
 }
 
